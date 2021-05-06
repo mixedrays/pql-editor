28ae627a9dabf1b5e25f681dbd372ffb4414aad2
AMialeshka <amialeshka.external@epo.org>
2021-05-06 10:46:12 +0200

    Update PQL syntax highlighting

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -8,12 +8,73 @@ const Editor: React.FC<{ language: string; }> = ({language}) => {
     divNode = node;
   }, []);
 
+  monaco.editor.defineTheme('pqlTheme', {
+    base: "vs",
+    inherit: true,
+    rules: [
+      {
+        "background": "F8F8FF",
+        "token": ""
+      },
+      {
+        "foreground": "a61717",
+        "background": "e3d2d2",
+        "token": "invalid.illegal"
+      },
+      {
+        "foreground": "999988",
+        "token": "comment"
+      },
+      {
+        "foreground": "536dfe",
+        "token": "keyword"
+      },
+      {
+        "foreground": "986801",
+        "token": "excludeOperator"
+      },
+      {
+        "foreground": "d500f9",
+        "token": "operator"
+      },
+      {
+        "foreground": "00b0ff",
+        "token": "qualifier"
+      },
+      {
+        "foreground": "00b0ff",
+        "token": "hierarchyOperator"
+      },
+      {
+        "foreground": "d500f9",
+        "token": "separator"
+      },
+      {
+        "foreground": "d500f9",
+        "token": "delimiter"
+      },
+      {
+        "foreground": "43a047",
+        "token": "number"
+      },
+    ],
+    colors: {
+      "editor.foreground": "#000000",
+      "editor.background": "#FFFFFF",
+      "editor.selectionBackground": "#B4D5FE",
+      "editor.lineHighlightBackground": "#EEEEEE",
+      "editorCursor.foreground": "#666666",
+      "editorWhitespace.foreground": "#BBBBBB"
+    }
+  });
+
   React.useEffect(() => {
     if (!divNode) return;
 
     monaco.editor.create(divNode, {
       value: '',
       language,
+      theme: 'pqlTheme',
       minimap: {enabled: false},
       autoIndent: true
     });
--- src/pql-lang/pqlLangMonarch.ts
+++ src/pql-lang/pqlLangMonarch.ts
@@ -2,40 +2,72 @@ import * as monaco from "monaco-editor-core";
 import IRichLanguageConfiguration = monaco.languages.LanguageConfiguration;
 import ILanguage = monaco.languages.IMonarchLanguage;
 
-export const richLanguageConfiguration: IRichLanguageConfiguration = {};
+export const richLanguageConfiguration: IRichLanguageConfiguration = {
+  comments: {
+    blockComment: ['{', '}']
+  },
+};
 
 export const monarchLanguage = <ILanguage>{
   defaultToken: 'invalid',
 
   keywords: ['AND', 'OR', 'nUG', 'nOG', 'nD', 'nW', 'S', 'L', 'P', 'F', 'IN', 'YES', 'NO'],
 
-  typeKeywords: ['NOT', 'NOV', '!nD', '!nW', '!S', '!L', '!P', '!F', '!IN'],
+  excludingOperators: ['NOT', 'NOV', '!nD', '!nW', '!S', '!L', '!P', '!F', '!IN'],
 
-  operators: [',', '=', '>', '>=', '<', '<=', ')', '(', '|', '&'],
+  qualifiers: ['EN', 'FR', 'DE', 'CPC'],
+
+  hierarchyOperators: ['HIGH', 'LOW', 'SAME'],
+
+  operators: ['=', '>', '>=', '<', '<=', '|', '&', '~', '@', '/', '!', '^', '?', '+', '#', '$'],
+
+  separators: [',', ':', '-', '_'],
+
+  symbols: /[_,#@=><!~?:&|+\-*\/\^%$]/,
 
   brackets: [
-    {open: '{', close: '}', token: 'delimiter.curly'},
+    // {open: '{', close: '}', token: 'delimiter.curly'},
     {open: '[', close: ']', token: 'delimiter.square'},
     {open: '(', close: ')', token: 'delimiter.parenthesis'},
-    {open: '<', close: '>', token: 'delimiter.angle'}
+    // {open: '<', close: '>', token: 'delimiter.angle'}
   ],
 
   escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
 
   tokenizer: {
     root: [
+      // delimiters and operators
+      [/[()\[\]]/, '@brackets'],
+      [/@symbols/, {
+        cases: {
+          '@operators': 'operator',
+          '@separators': 'separator',
+          '@default': ''
+        }
+      }],
+
       // identifiers and keywords
-      [/[a-zA-Z_$][\w$]*/, {
+      [/[a-zA-Z][\w$]*/, {
         cases: {
           '@keywords': {token: 'keyword'},
-          '@typeKeywords': {token: 'type'},
+          '@excludingOperators': {token: 'excludeOperator'},
+          '@qualifiers': {token: 'qualifier'},
+          '@hierarchyOperators': {token: 'hierarchyOperator'},
           '@default': 'identifier'
         }
       }],
 
+      // digits
+      [/\d+/, 'number'],
+
       // whitespace
       {include: '@whitespace'},
+
+      //comments
+      [/{/, 'comment', '@comment'],
     ],
+
+    comment: [[/}/, 'comment', '@pop'], [/./, 'comment.content']],
 
     whitespace: [
       [/[ \t\r\n]+/, ''],
